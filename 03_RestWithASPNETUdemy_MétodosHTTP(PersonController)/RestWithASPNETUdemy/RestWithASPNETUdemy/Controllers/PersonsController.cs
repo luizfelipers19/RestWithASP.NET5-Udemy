87c96e21e3651f46e3545e729c5db22629fc003c
LuizFelipe <luizfelipers19@gmail.com>
2021-04-10 23:13:01 -0300

    Created new project to use the other HTTP methods

---
+++ 03_RestWithASPNETUdemy_MétodosHTTP(PersonController)/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonsController : ControllerBase
+    {
+      
+
+        private readonly ILogger<PersonsController> _logger;
+
+        public PersonsController(ILogger<PersonsController> logger)
+        {
+            _logger = logger;
+        }
+
+       
+    }
+}
