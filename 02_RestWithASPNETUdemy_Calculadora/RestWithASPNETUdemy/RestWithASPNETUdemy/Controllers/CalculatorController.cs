1b555a6e0e8003937199de04ba4a999ddc3a5473
LuizFelipe <luizfelipers19@gmail.com>
2021-04-10 21:33:01 -0300

    projeto calculadora - limpeza do boilerplate e setup inicial

---
+++ 02_RestWithASPNETUdemy_Calculadora/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber)  + ConvertToDecimal(secondNumber) ;
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        private bool IsNumeric(string firstNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
