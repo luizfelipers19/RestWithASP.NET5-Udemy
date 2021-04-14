0ef2db1d3f1753af4fc54e093cdf9f7ac8b57e3e
LuizFelipe <luizfelipers19@gmail.com>
2021-04-13 22:36:41 -0300

    exported the sql database and splitted the file in 2: 1 for the creation of the dataset(table) and the other for populating the table with data(insert). Also applied this same logic to a new table of Books

--- 05_RestWithASPNETUdemy_EntityFrameworkMigrations/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
+++ 05_RestWithASPNETUdemy_EntityFrameworkMigrations/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -16,27 +16,39 @@ using System.Linq;
 using System.Threading.Tasks;
 using RestWithASPNETUdemy.Repository.Implementations;
 using RestWithASPNETUdemy.Repository;
+using Serilog;
 
 namespace RestWithASPNETUdemy
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IWebHostEnvironment Environment { get;  }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
+
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
         }
 
-        public IConfiguration Configuration { get; }
+        
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
+            
+
             services.AddControllers();
 
             var connection = Configuration["MySQLConnection:MySQLConnectionString"];
             services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
-
+             if (Environment.IsDevelopment())
+             {
+                MigrateDatabase(connection);
+             }
             //Api versioning
             services.AddApiVersioning();
 
@@ -45,6 +57,8 @@ namespace RestWithASPNETUdemy
             services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
             services.AddScoped<IPersonRepository, PersonRepositoryImplementation>();
         }
+
+        
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -64,6 +78,24 @@ namespace RestWithASPNETUdemy
             {
                 endpoints.MapControllers();
             });
+        }
+        private void MigrateDatabase(string connection)
+        {
+            try
+            {
+                var evolveConnection = new MySql.Data.MySqlClient.MySqlConnection(connection);
+                var evolve = new Evolve.Evolve(evolveConnection, msg => Log.Information(msg))
+                {
+                    Locations = new List<string> { "db/migrations", "db/dataset" },
+                    IsEraseDisabled = true,
+                };
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Database Migration Failed", ex);
+                throw;
+            }
         }
     }
 }
