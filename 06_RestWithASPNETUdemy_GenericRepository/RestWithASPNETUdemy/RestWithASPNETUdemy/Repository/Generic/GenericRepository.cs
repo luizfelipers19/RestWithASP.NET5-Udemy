153031af212624ebc2694ddd44f83af35b984ee8
LuizFelipe <luizfelipers19@gmail.com>
2021-04-16 12:22:23 -0300

    implemented the generic repository and will start adding the specific operations to each model class soon

--- 06_RestWithASPNETUdemy_GenericRepository/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/Generic/GenericRepository.cs
+++ 06_RestWithASPNETUdemy_GenericRepository/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/Generic/GenericRepository.cs
@@ -1,40 +1,101 @@
-﻿using RestWithASPNETUdemy.Model.Base;
+﻿using Microsoft.EntityFrameworkCore;
+using RestWithASPNETUdemy.Model.Base;
+using RestWithASPNETUdemy.Model.Context;
 using System;
 using System.Collections.Generic;
-
+using System.Linq;
 
 namespace RestWithASPNETUdemy.Repository.Generic
 {
     public class GenericRepository<T> : IRepository<T> where T : BaseEntity
     {
+        private MySQLContext  _context;
+
+        private DbSet<T> dataset;
+
+        public GenericRepository(MySQLContext context)
+        {
+            _context = context;
+            dataset = _context.Set<T>();
+        }
+
         public T Create(T item)
         {
-            throw new NotImplementedException();
+            try
+            {
+                dataset.Add(item);
+                _context.SaveChanges();
+                return item;
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+            
         }
 
         public void Delete(long id)
         {
-            throw new NotImplementedException();
+            var result = dataset.SingleOrDefault(p => p.Id.Equals(id));
+
+            if (result != null)
+            {
+
+                try
+                {
+                    dataset.Remove(result);
+                    _context.SaveChanges();
+                    
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+            }
         }
 
         public bool Exists(long id)
         {
-            throw new NotImplementedException();
+            return dataset.Any(p => p.Id.Equals(id));
         }
 
         public List<T> FindAll()
         {
-            throw new NotImplementedException();
+            return dataset.ToList();
         }
 
         public T FindById(long id)
         {
-            throw new NotImplementedException();
+            return dataset.SingleOrDefault(p => p.Id.Equals(id));
         }
 
         public T Update(T item)
         {
-            throw new NotImplementedException();
+            var result = dataset.SingleOrDefault(p => p.Id.Equals(item.Id));
+
+            if (result != null)
+            {
+
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(item);
+                    _context.SaveChanges();
+                    return result;
+
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
