0461c7b478a7e2f06e0fd540e61fef8aecc39bb1
LuizFelipe <luizfelipers19@gmail.com>
2021-04-16 23:06:04 -0300

    created the folder to hold the next step of the course: Implement the Value-Object project pattern on the API

---
+++ 07_RestWithASPNETUdemy_ValueObject/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/IBookBusiness.cs
@@ -0,0 +1,25 @@
+﻿using RestWithASPNETUdemy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Business
+{
+    public interface IBookBusiness
+    { 
+       Book Create(Book book);
+
+        Book FindById(long id);
+
+        List<Book> FindAll();
+
+        Book Update(Book book);
+
+        void Delete(long id);
+
+       
+
+
+    }
+}
---
+++ 07_RestWithASPNETUdemy_ValueObject/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/IPersonBusiness.cs
@@ -0,0 +1,25 @@
+﻿using RestWithASPNETUdemy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Business
+{
+    public interface IPersonBusiness
+    { 
+        Person Create(Person person);
+
+        Person FindById(long id);
+
+        List<Person> FindAll();
+
+        Person Update(Person person);
+
+        void Delete(long id);
+
+       
+
+
+    }
+}
