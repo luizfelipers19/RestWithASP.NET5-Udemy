3baeac5b061a54ad7834ee3527e4ae4443416014
LuizFelipe <luizfelipers19@gmail.com>
2021-04-18 21:53:46 -0300

    started to implement the Value Object design pattern by creating the Converter folder and the IParser class, along with the PersonConverter that implements the convertion between the person obj into person value object

---
+++ 07_RestWithASPNETUdemy_ValueObject/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -0,0 +1,30 @@
+﻿using RestWithASPNETUdemy.Model.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Data.VO
+{
+
+   
+    public class PersonVO
+    {
+        public long Id { get; set; }
+
+        
+        public string FirstName { get; set; }
+
+        
+        public string LastName { get; set; }
+
+        
+        public string Address { get; set; }
+
+        
+        public string Gender { get; set; }
+
+
+    }
+}
