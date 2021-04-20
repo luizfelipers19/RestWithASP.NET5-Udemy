3b34b9cf19977c12b3ea2b403723fba0603b1efd
LuizFelipe <luizfelipers19@gmail.com>
2021-04-20 10:46:49 -0300

    added the Hypermedia folder as long as interfaces and constants to build the HATEOAS structure

---
+++ 08_RestWithASPNETUdemy_Hateoas/RestWithASPNETUdemy/RestWithASPNETUdemy/Hypermedia/Abstract/IResponseEnricher.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Hypermedia.Abstract
+{
+    public interface IResponseEnricher
+    {
+        bool CanEnrich(ResultExecutedContext context)
+
+
+        Task Enrich(ResultExecutedContext context);
+    }
+}
---
+++ 08_RestWithASPNETUdemy_Hateoas/RestWithASPNETUdemy/RestWithASPNETUdemy/Hypermedia/Abstract/ISupportHypermedia.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RestWithASPNETUdemy.Hypermedia.Abstract
+{
+    public interface ISupportHypermedia
+    {
+        List<HyperMediaLink> Links { get; set; }
+    }
+}
