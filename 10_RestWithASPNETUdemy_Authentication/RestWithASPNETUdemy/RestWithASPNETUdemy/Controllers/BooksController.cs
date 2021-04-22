1be5f44ca86e0db68392d4684861ae9155023fb7
LuizFelipe <luizfelipers19@gmail.com>
2021-04-22 08:50:40 -0300

    created new folder to start the implementation of Authentication and Authorization

---
+++ 10_RestWithASPNETUdemy_Authentication/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/BooksController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Hypermedia.Filters;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class BooksController : ControllerBase
+    {
+      
+
+        private readonly ILogger<BooksController> _logger;
+        private IBookBusiness _bookBusiness;
+
+        public BooksController(ILogger<BooksController> logger, IBookBusiness bookBusiness)
+        {
+            _logger = logger;
+            _bookBusiness = bookBusiness;
+        }
+
+        [HttpGet]
+        [ProducesResponseType((200), Type = typeof(List<BookVO>))]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get()
+        {
+            return Ok(_bookBusiness.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType((200), Type = typeof(BookVO))]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get(long id)
+        {
+
+            var book = _bookBusiness.FindById(id);
+            if (book == null) return NotFound();
+            return Ok(book);
+        }
+
+        [HttpPost]
+        [ProducesResponseType((200), Type = typeof(BookVO))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Post([FromBody] BookVO book)
+        {
+
+            if (book == null) return BadRequest();
+            return Ok(_bookBusiness.Create(book));
+        }
+
+        [HttpPut]
+        [ProducesResponseType((200), Type = typeof(BookVO))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Put([FromBody] BookVO book)
+        {
+            if (book == null) return BadRequest();
+            return Ok(_bookBusiness.Update(book));
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        public IActionResult Delete(long id)
+        {
+             _bookBusiness.Delete(id);
+            
+            return NoContent();
+        }
+
+    }
+}
