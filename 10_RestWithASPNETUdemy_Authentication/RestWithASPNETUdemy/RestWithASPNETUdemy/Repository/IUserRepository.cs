27690d103288322be78a5b44c01ca51fb72092e5
LuizFelipe <luizfelipers19@gmail.com>
2021-04-22 16:26:46 -0300

    Created the UserRepository with an IUserRepository interface and an UserVO. Implemented the basic of validatingCredentials and hashing the password

---
+++ 10_RestWithASPNETUdemy_Authentication/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Repository
+{
+    public interface IUserRepository
+    {
+        User ValidateCredentials(UserVO user);  
+
+    }
+}
---
+++ 10_RestWithASPNETUdemy_Authentication/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly MySQLContext _context;
+
+        public UserRepository(MySQLContext context)
+        {
+            _context = context;
+        }
+
+        public User ValidateCredentials(UserVO user)
+        {
+
+            var pass = ComputeHash(user.Password, new SHA256CryptoServiceProvider());
+            return _context.Users.FirstOrDefault(u => (u.UserName == user.UserName) && (u.Password == pass));
+        }
+
+        private string ComputeHash(string input, SHA256CryptoServiceProvider algorithm)
+        {
+            Byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+            Byte[] hashedBytes = algorithm.ComputeHash(inputBytes);
+            return BitConverter.ToString(hashedBytes);
+        }
+    }
+}
