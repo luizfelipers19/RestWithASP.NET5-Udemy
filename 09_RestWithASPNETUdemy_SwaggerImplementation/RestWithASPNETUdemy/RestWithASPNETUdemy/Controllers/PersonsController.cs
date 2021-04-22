7d722bd8ca999f785bf6b1d9e606e2c6426881f9
LuizFelipe <luizfelipers19@gmail.com>
2021-04-21 21:47:11 -0300

    created a new Project Folder for the next step of the project: Swagger implementation

---
+++ 09_RestWithASPNETUdemy_SwaggerImplementation/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/PersonsController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Hypermedia.Filters;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    public class PersonsController : ControllerBase
+    {
+      
+
+        private readonly ILogger<PersonsController> _logger;
+        private IPersonBusiness _personBusiness;
+
+        public PersonsController(ILogger<PersonsController> logger, IPersonBusiness personBusiness)
+        {
+            _logger = logger;
+            _personBusiness = personBusiness;
+        }
+
+        [HttpGet]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get()
+        {
+            return Ok(_personBusiness.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Get(long id)
+        {
+
+            var person = _personBusiness.FindById(id);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        [HttpPost]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Post([FromBody] PersonVO person)
+        {
+
+            if (person == null) return BadRequest();
+            return Ok(_personBusiness.Create(person));
+        }
+
+        [HttpPut]
+        [TypeFilter(typeof(HyperMediaFilter))]
+        public IActionResult Put([FromBody] PersonVO person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personBusiness.Update(person));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+             _personBusiness.Delete(id);
+            
+            return NoContent();
+        }
+
+    }
+}
