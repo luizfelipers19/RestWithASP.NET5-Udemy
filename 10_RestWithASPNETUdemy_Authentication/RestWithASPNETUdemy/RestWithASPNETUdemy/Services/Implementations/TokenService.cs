fcb0105f3c7c1eb49ae731e3e26f962fe3a08799
LuizFelipe <luizfelipers19@gmail.com>
2021-04-23 11:37:15 -0300

    implemented all methods of the TokenService class

--- 10_RestWithASPNETUdemy_Authentication/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/TokenService.cs
+++ 10_RestWithASPNETUdemy_Authentication/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/TokenService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -39,12 +40,35 @@ namespace RestWithASPNETUdemy.Services.Implementations
 
         public string GenerateRefreshToken()
         {
-            throw new NotImplementedException();
+
+            var randomNumber = new byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomNumber);
+                return Convert.ToBase64String(randomNumber);
+            };
+                
         }
 
-        public ClaimsPrincipal GetPrincipalFromExpiredToken(string Token)
+        public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
         {
-            throw new NotImplementedException();
+
+            var tokenValidationParameters = new TokenValidationParameters { 
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.Secret)),
+                ValidateLifetime = false
+        };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken securityToken;
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out securityToken );
+            var jwtSecurityToken = securityToken as JwtSecurityToken;
+            if (jwtSecurityToken == null ||
+                !jwtSecurityToken.Header.Alg.Equals(
+                    SecurityAlgorithms.HmacSha256,
+                    StringComparison.InvariantCulture)) throw new SecurityTokenException("Invalid Token");
+            return principal;
         }
     }
 }
