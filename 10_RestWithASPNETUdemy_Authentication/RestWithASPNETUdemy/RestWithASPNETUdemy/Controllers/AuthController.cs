e4dd91c1feba18799809147dabaf35a8f37a4004
LuizFelipe <luizfelipers19@gmail.com>
2021-04-24 19:11:40 -0300

    created the AuthController controller class with a SignIn method and added the [Authorize] anotation on both Books and Persons controllers

---
+++ 10_RestWithASPNETUdemy_Authentication/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Data.VO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private ILoginBusiness _loginBusiness;
+
+        public AuthController(ILoginBusiness loginBusiness)
+        {
+            _loginBusiness = loginBusiness;
+        }
+
+        [HttpPost]
+        [Route("signin")]
+        public IActionResult SignIn([FromBody] UserVO user)
+        {
+            if (user == null) return BadRequest("Invalid client request");
+            var token = _loginBusiness.ValidateCredentials(user);
+            if (token == null) return Unauthorized();
+
+            return Ok(token);
+        }
+    }
+}
--- 10_RestWithASPNETUdemy_Authentication/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/BooksController.cs
+++ 10_RestWithASPNETUdemy_Authentication/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/BooksController.cs
@@ -8,12 +8,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using RestWithASPNETUdemy.Data.VO;
 using RestWithASPNETUdemy.Hypermedia.Filters;
+using Microsoft.AspNetCore.Authorization;
 
 namespace RestWithASPNETUdemy.Controllers
 {
 
     [ApiVersion("1")]
     [ApiController]
+    [Authorize("Bearer")]
     [Route("api/[controller]/v{version:apiVersion}")]
     public class BooksController : ControllerBase
     {
