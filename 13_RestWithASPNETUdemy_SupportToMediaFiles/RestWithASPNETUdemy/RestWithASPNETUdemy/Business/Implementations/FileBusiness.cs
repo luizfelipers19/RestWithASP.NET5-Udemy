257103dfa83e9d211d481cd265018975e9edfafe
LuizFelipe <luizfelipers19@gmail.com>
2021-05-02 10:23:30 -0300

    created the IFileBusiness interface and the FileBusiness class to implement the interface, but just created them. It will support the upload of a file and of a list of files

---
+++ 13_RestWithASPNETUdemy_SupportToMediaFiles/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/Implementations/FileBusiness.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using RestWithASPNETUdemy.Data.VO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Business.Implementations
+{
+    public class FileBusiness : IFileBusiness
+    {
+        private readonly string _basePath;
+        private readonly IHttpContextAccessor _context;
+
+        public FileBusiness(IHttpContextAccessor context)
+        {
+            _context = context;
+            _basePath = Directory.GetCurrentDirectory() + "\\UploadDir\\";
+        }
+
+        public byte[] GetFile(string filename)
+        {
+            
+
+            throw new NotImplementedException();
+        }
+
+        public Task<List<FileDetailVO>> SaveFilesToDisk(IList<IFormFile> file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FileDetailVO> SaveFileToDisk(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
