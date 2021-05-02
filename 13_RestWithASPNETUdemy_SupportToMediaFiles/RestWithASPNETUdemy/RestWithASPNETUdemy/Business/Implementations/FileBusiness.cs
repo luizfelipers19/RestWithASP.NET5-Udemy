163c6302f523e717a79bef6795e07ad740d6a2a4
LuizFelipe <luizfelipers19@gmail.com>
2021-05-02 11:36:43 -0300

    implemented the method responsible for saving the file to Disk

--- 13_RestWithASPNETUdemy_SupportToMediaFiles/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/Implementations/FileBusiness.cs
+++ 13_RestWithASPNETUdemy_SupportToMediaFiles/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/Implementations/FileBusiness.cs
@@ -25,15 +25,34 @@ namespace RestWithASPNETUdemy.Business.Implementations
 
             throw new NotImplementedException();
         }
+         public async Task<FileDetailVO> SaveFileToDisk(IFormFile file)
+                {
+            FileDetailVO fileDetail = new FileDetailVO();
+            var fileType = Path.GetExtension(file.FileName);
+            var baseUrl = _context.HttpContext.Request.Host;
+
+            if (fileType.ToLower() == ".pdf" || fileType.ToLower() == ".jpg" || fileType.ToLower() == ".jpeg" || fileType.ToLower() == ".png")
+            {
+                var docName = Path.GetFileName(file.FileName);
+                if (file != null && file.Length > 0)
+                {
+                    var destination = Path.Combine(_basePath, "", docName);
+                    fileDetail.DocumentName = docName;
+                    fileDetail.DocType = fileType;
+                    fileDetail.DocUrl = Path.Combine(baseUrl + "/api/file/v1" + fileDetail.DocumentName);
+
+                    using var stream = new FileStream(destination, FileMode.Create);
+                    await file.CopyToAsync(stream);
+                }
+            }
+            return fileDetail;
+                }
 
         public Task<List<FileDetailVO>> SaveFilesToDisk(IList<IFormFile> file)
         {
             throw new NotImplementedException();
         }
 
-        public Task<FileDetailVO> SaveFileToDisk(IFormFile file)
-        {
-            throw new NotImplementedException();
-        }
+       
     }
 }
