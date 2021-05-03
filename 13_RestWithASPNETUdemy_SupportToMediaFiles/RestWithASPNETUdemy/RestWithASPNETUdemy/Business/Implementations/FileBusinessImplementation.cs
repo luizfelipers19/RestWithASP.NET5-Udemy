1ad1b166d0d1df8219809866938cf2750b48b6f9
LuizFelipe <luizfelipers19@gmail.com>
2021-05-03 09:34:09 -0300

    added the support to download the files via Postman, but we need to use the Save Response option in Postman to be able to get the document file, because it only returns the bynary code of the file

--- 13_RestWithASPNETUdemy_SupportToMediaFiles/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/Implementations/FileBusinessImplementation.cs
+++ 13_RestWithASPNETUdemy_SupportToMediaFiles/RestWithASPNETUdemy/RestWithASPNETUdemy/Business/Implementations/FileBusinessImplementation.cs
@@ -21,9 +21,10 @@ namespace RestWithASPNETUdemy.Business.Implementations
 
         public byte[] GetFile(string filename)
         {
-            
 
-            throw new NotImplementedException();
+            var filePath = _basePath + filename;
+            return File.ReadAllBytes(filePath);
+           
         }
          public async Task<FileDetailVO> SaveFileToDisk(IFormFile file)
                 {
--- 13_RestWithASPNETUdemy_SupportToMediaFiles/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/FileController.cs
+++ 13_RestWithASPNETUdemy_SupportToMediaFiles/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/FileController.cs
@@ -5,6 +5,7 @@ using RestWithASPNETUdemy.Business;
 using RestWithASPNETUdemy.Data.VO;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -35,6 +36,28 @@ namespace RestWithASPNETUdemy.Controllers
         {
             FileDetailVO detail = await _fileBusiness.SaveFileToDisk(file);
             return new OkObjectResult(detail);
+        } 
+        
+        [HttpGet("downloadFile/{fileName}")]
+        [ProducesResponseType((200), Type = typeof(byte[]))]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [Produces("application/octet-stream")]
+        
+        //realiza o upload de um único arquivo
+        public async Task<IActionResult> GetFileAsync(string fileName)
+        {
+            byte[] buffer = _fileBusiness.GetFile(fileName);
+            // FileDetailVO detail = await 
+            if (buffer != null)
+            {
+                HttpContext.Response.ContentType = 
+                    $"application/{ Path.GetExtension(fileName).Replace(".","")}";
+                HttpContext.Response.Headers.Add("content-length", buffer.Length.ToString());
+                await HttpContext.Response.Body.WriteAsync(buffer, 0, buffer.Length);
+            }
+            return new ContentResult();
         }
         
         
